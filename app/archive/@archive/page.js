49cc57bce574cea6c5f5b0c4e7dbd59c972113ec
VeldinS <veldinsalcinovic123@gmail.com>
2024-05-11 12:55:46 +0200

    Implementation of news page, news details page and archive parallel routes

---
+++ app/archive/@archive/page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ArchivePage() {
+    return (
+        <h1>Archive</h1>
+    );
+}
+
+export default ArchivePage;
---
+++ app/archive/@latest/page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LatestNewsPage() {
+    return (
+        <h1>Latest News Page</h1>
+    );
+}
+
+export default LatestNewsPage;
