1194b6a89d94fcb23cfaaaf2981b56d9bd8488b7
VeldinS <veldinsalcinovic123@gmail.com>
2024-05-11 18:35:24 +0200

    Filter for years and months. Error handling.

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import NewsList from "@/components/news-list";
-import {getAvailableNewsYears, getNewsForYear} from "@/lib/news";
+import {getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth} from "@/lib/news";
 import Link from "next/link";
 
-function FilteredNewsPage(params) {
+function FilteredNewsPage({params}) {
 
     const filter = params.filter;
 
@@ -11,29 +11,45 @@ function FilteredNewsPage(params) {
     const selectedMonth = filter?.[1];
 
     let news;
+    let links = getAvailableNewsYears();
 
-    if(selectedYear && !selectedMonth){
-        news = getNewsForYear(selectedYear)
+    if (selectedYear && !selectedMonth) {
+        news = getNewsForYear(selectedYear);
+        links = getAvailableNewsMonths(selectedYear);
     }
 
-    let newsContent = <p>No news found for the  selected period.</p>
-
-    if(news && news.length > 0){
-        newsContent = <NewsList news={news} />
+    if (selectedYear && selectedMonth) {
+        news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+        links = [];
     }
 
-    const links = getAvailableNewsYears();
+    let newsContent = <p>No news found for the selected period.</p>;
+
+    if (news && news.length > 0) {
+        newsContent = <NewsList news={news} />;
+    }
+
+    if((selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+        (selectedMonth && !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))){
+        throw new Error('Invalid filter');
+    }
 
     return (
         <>
             <header id={"archive-header"}>
                 <nav>
                     <ul>
-                        {links.map((link) => (
-                            <li key={link}>
-                                <Link href={`/archive/${link}`}>{link}</Link>
-                            </li>
-                        ))}
+                        {links.map((link) => {
+                            const href = selectedYear
+                                ? `/archive/${selectedYear}/${link}`
+                                : `/archive/${link}`;
+
+                            return (
+                                <li key={link}>
+                                    <Link href={href}>{link}</Link>
+                                </li>
+                            );
+                        })}
                     </ul>
                 </nav>
             </header>
--- components/news-list.js
+++ components/news-list.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 function NewsList() {
     return (
         <ul className={"news-list"}>
-            {DUMMY_NEWS.map(newsItem => (
+            {DUMMY_NEWS.map((newsItem) => (
                 <li key={newsItem.id}>
                     <Link href={`/news/${newsItem.slug}`}>
                         <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/>
